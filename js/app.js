b88c8381fe33355a1aec7927fa932ed88a6e1a2c
CherieLee-Mason <clsoar@gmail.com>
2019-02-11 16:30:07 -0600

    Mobile menu and sub completed

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
 const hamburgerMenu = document.querySelector("#hamburger-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const hamburger = document.querySelector(".hamburger");
+const servicesSelector = document.querySelector("#services-item");
+const mobileServicesMenu = document.querySelector(".mobile-services-menu");
+const mobileMenuBack = document.querySelector("#back-btn");
+
+
 
 hamburgerMenu.addEventListener("click", (evt) => {
   mobileMenu.classList.toggle("hidden");
   hamburger.classList.toggle("hamburger-close");
+
+})
+
+servicesSelector.addEventListener("click", (evt) => {
+
+  mobileServicesMenu.classList.toggle("hidden");
+  mobileServicesMenu.classList.toggle("show-submenu");
+
+})
+
+mobileMenuBack.addEventListener("click", (evt) => {
+  mobileServicesMenu.classList.toggle("hidden");
+  mobileServicesMenu.classList.toggle("show-submenu");
+  console.log("it's clicked");
 })

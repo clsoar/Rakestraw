6af660a38e0dd3bec78238a33b6e8d35104324b3
CherieLee-Mason <clsoar@gmail.com>
2019-03-24 21:41:10 -0500

    Modal open and close functions

--- js/app.js
+++ js/app.js
@@ -4,7 +4,10 @@ const hamburger = document.querySelector(".hamburger");
 const servicesSelector = document.querySelector("#services-item");
 const mobileServicesMenu = document.querySelector(".mobile-services-menu-list");
 const mobileMenuBack = document.querySelector("#back-btn");
-
+const submitModal = document.querySelector(".submit-modal-wrapper");
+const modalOverlay = document.querySelector(".modal-overlay");
+const formSubmitBtn = document.querySelector("#form-submit-btn");
+const modalCloseBtn = document.querySelector("#modal-close");
 
 
 hamburgerMenu.addEventListener("click", (evt) => {
@@ -14,15 +17,29 @@ hamburgerMenu.addEventListener("click", (evt) => {
     mobileServicesMenu.classList.toggle("hidden");
     mobileServicesMenu.classList.toggle("show-submenu");
   }
-})
+});
 
 servicesSelector.addEventListener("click", (evt) => {
   mobileServicesMenu.classList.toggle("hidden");
   mobileServicesMenu.classList.toggle("show-submenu");
-})
+});
 
 mobileMenuBack.addEventListener("click", (evt) => {
   mobileServicesMenu.classList.toggle("hidden");
   mobileServicesMenu.classList.toggle("show-submenu");
   console.log("it's clicked");
-})
+});
+
+formSubmitBtn.addEventListener("click",(evt) => {
+  evt.preventDefault();
+  modalOverlay.classList.remove("hidden");
+  submitModal.classList.remove("hidden");
+
+});
+
+modalCloseBtn.addEventListener("click",(evt) => {
+  evt.preventDefault();
+  modalOverlay.classList.add("hidden");
+  submitModal.classList.add("hidden");
+
+});

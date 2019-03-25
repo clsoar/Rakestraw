a7f9157b306ac89a1ac8c98ee09beb04d2f5735d
CherieLee-Mason <clsoar@gmail.com>
2019-03-25 09:51:43 -0500

    Trab focus on modal

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ const submitModal = document.querySelector(".submit-modal-wrapper");
 const modalOverlay = document.querySelector(".modal-overlay");
 const formSubmitBtn = document.querySelector("#form-submit-btn");
 const modalCloseBtn = document.querySelector("#modal-close");
-
+let focusedElementPreModal;
 
 hamburgerMenu.addEventListener("click", (evt) => {
   mobileMenu.classList.toggle("hidden");
@@ -32,14 +32,68 @@ mobileMenuBack.addEventListener("click", (evt) => {
 
 formSubmitBtn.addEventListener("click",(evt) => {
   evt.preventDefault();
-  modalOverlay.classList.remove("hidden");
-  submitModal.classList.remove("hidden");
+  openModal();
+  });
 
-});
+  //trab tab key code from https://developers.google.com/web/fundamentals/accessibility/focus/using-tabindex
+  openModal = () => {
 
-modalCloseBtn.addEventListener("click",(evt) => {
-  evt.preventDefault();
-  modalOverlay.classList.add("hidden");
-  submitModal.classList.add("hidden");
+    focusedElementPreModal = document.activeElement;
 
-});
+
+    //function to trap tab key in modal
+    trapTabKey = (evt) => {
+      if (evt.keyCode === 9) {
+        if (evt.shiftKey) {
+          if (document.activeElement === firstTabStop) {
+            evt.preventDefault();
+            lastTabStop.focus();
+          }
+        } else {
+          if (document.activeElement === lastTabStop) {
+            evt.preventDefault();
+            firstTabStop.focus();
+          }
+        }
+      }
+      //allow escape key to closemodal
+      if (evt.keyCode === 27) {
+        modalOverlay.classList.add("hidden");
+        submitModal.classList.add("hidden");
+        //set focus back to previously focused element
+        focusedElementPreModal.focus();
+      }
+    }
+
+    //open submission modal
+    modalOverlay.classList.remove("hidden");
+    submitModal.classList.remove("hidden");
+
+    //listen and trap tab key
+    submitModal.addEventListener('keydown', trapTabKey);
+
+    //close modal event listener
+    modalCloseBtn.addEventListener("click",(evt) => {
+      evt.preventDefault();
+      modalOverlay.classList.add("hidden");
+      submitModal.classList.add("hidden");
+      //set focus back to previously focused element
+      focusedElementPreModal.focus();
+    });
+
+    //focusable children string
+    let focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+    //select node list
+    let focusableElements = submitModal.querySelectorAll(focusableElementsString);
+    //convert to array
+    focusableElements = Array.prototype.slice.call(focusableElements);
+
+    let firstTabStop = focusableElements[0];
+    let lastTabStop = focusableElements[focusableElements.length - 1];
+
+    //set focus to first focusable element
+    firstTabStop.focus();
+
+
+
+  }

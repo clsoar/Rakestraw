89abc3ee47b3ec18a0730e2abef5191f903d64c1
CherieLee-Mason <clsoar@gmail.com>
2019-03-30 16:09:52 -0500

    Replaced popup with submission page, added spam protection

--- js/app.js
+++ js/app.js
@@ -8,6 +8,10 @@ const submitModal = document.querySelector(".submit-modal-wrapper");
 const modalOverlay = document.querySelector(".modal-overlay");
 const formSubmitBtn = document.querySelector("#form-submit-btn");
 const modalCloseBtn = document.querySelector("#modal-close");
+const extra = document.querySelector("#last-name");
+const extra2 = document.querySelector("#first-name");
+const extra3 = document.querySelector("#password");
+
 let focusedElementPreModal;
 
 hamburgerMenu.addEventListener("click", (evt) => {
@@ -30,73 +34,21 @@ mobileMenuBack.addEventListener("click", (evt) => {
   console.log("it's clicked");
 });
 
-/*
+//honeypot for form submission
+
 if(formSubmitBtn) {
-  formSubmitBtn.addEventListener("click", (evt) => {
-    openModal();
-  });
-}
+  //hide honeypot
+  extra.style.display = "none";
+  extra2.setAttribute('tabindex', "-1");
 
-
-//trab tab key code from https://developers.google.com/web/fundamentals/accessibility/focus/using-tabindex
-openModal = () => {
-
-  focusedElementPreModal = document.activeElement;
-
-
-  //function to trap tab key in modal
-  trapTabKey = (evt) => {
-    if (evt.keyCode === 9) {
-      if (evt.shiftKey) {
-        if (document.activeElement === firstTabStop) {
-          evt.preventDefault();
-          lastTabStop.focus();
-        }
+  if(!extra.value && !extra3.value){
+    formSubmitBtn.addEventListener("click", (evt) => {
+      if(!extra.value && extra2.value === "Do not change" && !extra3.value){
+        contactform.setAttribute('action', "https://www.briskforms.com/go/805c8ce203298ec4c019234a4bf7c5bb");
+        console.log("Form sent");
       } else {
-        if (document.activeElement === lastTabStop) {
-          evt.preventDefault();
-          firstTabStop.focus();
+        alert("Error in Form Submission, please try again.");
         }
-      }
-    }
-    //allow escape key to closemodal
-    if (evt.keyCode === 27) {
-      modalOverlay.classList.add("hidden");
-      submitModal.classList.add("hidden");
-      //set focus back to previously focused element
-      focusedElementPreModal.focus();
-    }
+    });
   }
-
-  //open submission modal
-  modalOverlay.classList.remove("hidden");
-  submitModal.classList.remove("hidden");
-
-  //listen and trap tab key
-  submitModal.addEventListener('keydown', trapTabKey);
-
-  //close modal event listener
-  modalCloseBtn.addEventListener("click",(evt) => {
-    evt.preventDefault();
-    modalOverlay.classList.add("hidden");
-    submitModal.classList.add("hidden");
-    //set focus back to previously focused element
-    focusedElementPreModal.focus();
-  });
-
-  //focusable children string
-  let focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
-  //select node list
-  let focusableElements = submitModal.querySelectorAll(focusableElementsString);
-  //convert to array
-  focusableElements = Array.prototype.slice.call(focusableElements);
-
-  let firstTabStop = focusableElements[0];
-  let lastTabStop = focusableElements[focusableElements.length - 1];
-
-  //set focus to first focusable element
-  firstTabStop.focus();
-
-
-
-}*/
+}

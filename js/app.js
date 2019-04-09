be0698b5cd423c1596c48aacf55c3873f8d6e910
CherieLee-Mason <clsoar@gmail.com>
2019-04-08 21:21:09 -0500

    Fixed mobile menu back button

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 const hamburgerMenu = document.querySelector("#hamburger-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const hamburger = document.querySelector(".hamburger");
-const servicesSelector = document.querySelector("#services-item");
+const servicesSelector = document.querySelector("#services-item a");
 const mobileServicesMenu = document.querySelector(".mobile-services-menu-list");
 const mobileMenuBack = document.querySelector("#back-btn");
+const mobileSubBackArrow = document.querySelector("#back-btn a");
 const submitModal = document.querySelector(".submit-modal-wrapper");
 const modalOverlay = document.querySelector(".modal-overlay");
 const formSubmitBtn = document.querySelector("#form-submit-btn");
@@ -18,21 +19,23 @@ hamburgerMenu.addEventListener("click", (evt) => {
   mobileMenu.classList.toggle("hidden");
   hamburger.classList.toggle("hamburger-close");
   if (mobileServicesMenu.classList.contains("show-submenu")) {
-    mobileServicesMenu.classList.toggle("hidden");
-    mobileServicesMenu.classList.toggle("show-submenu");
+    mobileServicesMenu.classList.add("hidden");
+    mobileServicesMenu.classList.remove("show-submenu");
   }
 });
 
 servicesSelector.addEventListener("click", (evt) => {
-  mobileServicesMenu.classList.toggle("hidden");
-  mobileServicesMenu.classList.toggle("show-submenu");
+  mobileServicesMenu.classList.remove("hidden");
+  mobileServicesMenu.classList.add("show-submenu");
+  console.log("open sub menu");
 });
 
 mobileMenuBack.addEventListener("click", (evt) => {
-  mobileServicesMenu.classList.toggle("hidden");
-  mobileServicesMenu.classList.toggle("show-submenu");
-  console.log("it's clicked");
+  mobileServicesMenu.classList.add("hidden");
+  mobileServicesMenu.classList.remove("show-submenu");
+  console.log("arrow is clicked");
 });
+
 
 //honeypot for form submission
 const contactform =  document.getElementById('contact-form');
